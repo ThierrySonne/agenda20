1125687d3a8b74cfde7fb4fd9596ff087cd18cfc
Sonne <thierryflowers@gmail.com>
2025-07-09 17:55:02 -0300

    Criação openDatabaseDB.dart

--- lib/database/openDatabaseDB.dart
+++ lib/database/openDatabaseDB.dart
@@ -1,1 +1,14 @@
 
+import n sei oq;
+import n sei oq la;
+
+Future<Database> getDatabase() async {
+  final String path = join(await getDatabasesPath(), 'dbrpg.db');
+
+  return openDatabase(
+    path,
+    onCreate: (db, version){
+      db.execute(PersonagemDAO.sqlTabelaPersonagem);
+    },
+    version: 1);
+}
